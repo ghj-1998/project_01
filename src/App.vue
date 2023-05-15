b901da3349e074038ceddf2a1a16a31bca7ea60d
ghj <463223552@qq.com>
2023-05-15 17:13:46 +0800

    修改了App.vue中的代码

--- src/App.vue
+++ src/App.vue
@@ -20,13 +20,13 @@
 #nav {
   padding: 0px;
 
-  // a {
-  //   font-weight: bold;
-  //   color: #2c3e50;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
 
-  //   &.router-link-exact-active {
-  //     color: #42b983;
-  //   }
-  // }
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
